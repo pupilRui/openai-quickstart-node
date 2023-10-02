e544bdda3277cdeb8d5a8d8bcd030df15e0f764a
pupilRui <142526194+pupilRui@users.noreply.github.com>
2023-10-01 17:06:29 -0700

    integrate with python scripts

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,62 +1,46 @@
 import { Configuration, OpenAIApi } from "openai";
+import { spawn } from 'child_process';
+import { Console } from "console";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const pythonExecutable = 'venv/bin/python3';
+const pythonProcess = spawn(pythonExecutable, ['backend/cmd_based.py'], {
+  stdio: ['pipe', 'pipe', 'pipe', 'ipc']
 });
-const openai = new OpenAIApi(configuration);
+
+function sendToPythonAndGetResponse(question) {
+  return new Promise((resolve, reject) => {
+    // Listen for the Python script's response
+    pythonProcess.stdout.once('data', (data) => {
+      resolve(data.toString().trim());
+    });
+
+    // Handle potential errors
+    pythonProcess.stderr.once('data', (err) => {
+      reject(new Error(err.toString().trim()));
+    });
+
+    // Send the question to the Python script
+    pythonProcess.stdin.write(`${question}\n`);
+  });
+}
 
 export default async function (req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid question",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
+    const result = await sendToPythonAndGetResponse(question);
+    console.log(`Received from Python script: ${result}`);
+    res.status(200).json({ result });
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ error: error.message });
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [questionInput, setquestionInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ question: questionInput }),
       });
 
       const data = await response.json();
@@ -23,7 +23,7 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setquestionInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -40,16 +40,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Customer Support System</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="question"
+            placeholder="Enter an question"
+            value={questionInput}
+            onChange={(e) => setquestionInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="submit question" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
